28e72776af3e673be7ce8c177b151ad224aa22a0
CDiala <dialachibuzo@yahoo.com>
2022-06-18 12:25:03 +0100

    Implemented backward navigation

--- src/components/widget/StepperControl.js
+++ src/components/widget/StepperControl.js
@@ -1,17 +1,18 @@
 import Stepper from "../shared/Stepper";
-import { stepCount } from "../../data/stepperCount";
 import { stepperControlContainerStyle } from "./stepperControlStyles";
 
-const StepperControl = ({ count }) => {
+const StepperControl = ({ count, stepCount, setCount }) => {
   return (
     <div className={stepperControlContainerStyle()}>
       {stepCount.map((step) => {
         return (
           <Stepper
             key={step}
+            id={`Step${step}`}
             isActive={step <= count + 1}
             step={step}
             length={stepCount.length}
+            setCount={setCount}
           />
         );
       })}
